4387c816881896256057dac4df52ae524be71fb1
Niko van der Vorst <niko.vander.vorst@capgemini.com>
2019-02-13 09:22:32 +0100

    first

---
+++ src/main/java/com/capgemini/oefenAssessment/OefenAssessmentApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.oefenAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OefenAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OefenAssessmentApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/capgemini/oefenAssessment/OefenAssessmentApplicationTests.java
@@ -0,0 +1,17 @@
+package com.capgemini.oefenAssessment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OefenAssessmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
